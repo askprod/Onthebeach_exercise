d640eab3118096bde0b6471ee7aa377866684103
Paul Trin-Dinh <paultrindinh@icloud.com>
2019-07-05 11:21:46 +0200

    Added methods to Jobs class: to sort out 2 arrays, to generate an array containing a to z, a random generated array for testing the program, and RSpec tests accordingly. Also added 2 error classes for handling them later

--- lib/jobs.rb
+++ lib/jobs.rb
@@ -1,5 +1,28 @@
-class Jobs
+class SameKeyError < StandardError
 
 end
 
-Jobs.new
+class CircularError < StandardError
+
+end
+
+class Jobs
+    def sort_jobs(a, b)
+        new_array = a.zip(b)
+    end
+
+    # Generates an array from a to z
+    def standard_alphabet_array
+        ('a'..'z').to_a
+    end
+
+    # Generates a random alphabet array with spaces for test purposes
+    def random_generated_alphabet_array
+        b =  (97..122).to_a.sort{ rand() - 0.5 }[0..26].map{|x| x.chr}
+        spaces = rand(0..26)
+        replacements = b.sample(spaces)
+        c = b.flat_map do |x|
+            x.split.map { |letter| replacements.include?(letter) ? " " : letter }
+        end
+    end
+end
--- spec/jobs_spec.rb
+++ spec/jobs_spec.rb
@@ -5,18 +5,23 @@ input = ("a".."f").to_a
 example = [" ", "c", "f", "a", "b", " "]
 same_key = [" ", " ", "c", " ", " ", " "]
 circular_dependency = [" ", "c", "f", "a", " ", "b"]
+standard_alphabet_array = Jobs.new.standard_alphabet_array
+random_generated_alphabet_array = Jobs.new.random_generated_alphabet_array
 
 describe 'jobs' do
     it "combines two arrays" do
-
+        array = Jobs.new.sort_jobs(input, example)
+        expect(array).to be_an_instance_of(Array)
     end
 
     it "should return a new random generated alphabet as an array" do
-
+        array = Jobs.new.random_generated_alphabet_array
+        expect(array).to be_an_instance_of(Array)
     end
 
     it "random generated alphabet should return a new alphabet array containing letters and spaces" do
-
+        array = Jobs.new.random_generated_alphabet_array
+        expect(array.flatten.to_s).to match(/[a-z\s]/)
     end 
 
     it "should return an error if a key == value" do
